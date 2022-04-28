500dce7dba75223b3511c02faa4ff490a1c985de
whj <1273803935@qq.com>
2022-04-28 14:38:09 +0800

    feat: change package.json

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -9,11 +9,16 @@ storiesOf('安装', module)
         <p>自定义的一套组件库，做的比较粗糙，持续完善中</p>
         <h3>安装:</h3>
         <code>
+          npm install whj_ui --save
+        </code>
+        <br/>
+        <h3>使用:</h3>
+        <code>
           import 'whj_ui/dist/index.css'
         </code>
         <br/>
         <code>
-          npm install whj_ui --save
+          {`import { Button } from 'whj_ui'`}
         </code>
         <br/>
         <code>
