8b1a15c2a8d5769322e22f396fc8d5af079141b7
whj <1273803935@qq.com>
2022-04-28 14:09:14 +0800

    feat: change readme.md

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -9,6 +9,10 @@ storiesOf('安装', module)
         <p>自定义的一套组件库，做的比较粗糙，持续完善中</p>
         <h3>安装:</h3>
         <code>
+          import 'whj_ui/dist/index.css'
+        </code>
+        <br/>
+        <code>
           npm install whj_ui --save
         </code>
         <br/>
